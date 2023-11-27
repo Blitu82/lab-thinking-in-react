6f7b758949d8ff3c731031e90be2d271649ab8c9
Blitu82 <garpaues@yahoo.es>
2023-11-27 18:11:26 +0000

    first round

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,22 @@
 function SearchBar({ onChange }) {
   return (
-    <nav className="navbar bg-body-tertiary d-flex justify-content-center">
-      <div className="container-fluid">
-        <form className="d-flex" role="search">
+    <nav className="navbar bg-body-tertiary">
+      <div className="container-fluid d-flex justify-content-center">
+        <form className="d-flex align-items-center" role="search">
           <input
             className="form-control me-2"
             type="text"
             placeholder="Search"
             aria-label="Search"
             onChange={onChange}
-          ></input>
+          />
+          <div className="form-check ms-2 mb-0">
+            <input className="form-check-input" type="checkbox" id="stock" />
+            <label className="form-check-label" htmlFor="stock">
+              Only show products in stock
+            </label>
+          </div>
         </form>
-      </div>
-      <div className="d-flex align-items-center">
-        <input type="checkbox" />
-        <label htmlFor="stock">Only show products in stock</label>
       </div>
     </nav>
   );
